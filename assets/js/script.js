0f128863e004aa12961d33abccd40b9009e4b7ea
ibalan92 <ibalan92@gmail.com>
2023-11-28 13:24:37 +0000

    Added function to display today's data

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var APIkey ="b7c8404eb7ec2db5624c4a872e9ce2d3"
 var historyEl = document.getElementById("history")
 var historyList = [];
 
+
 searchBtn.addEventListener("click", function(event){
     event.preventDefault();
     citySearched = city.value;
@@ -29,6 +30,8 @@ searchBtn.addEventListener("click", function(event){
         })
         .then(function(data) {
             console.log(data)
+            today(data);
+            console.log(data.list[0].weather[0].icon)
         });
     });
 
@@ -44,5 +47,26 @@ function checkHistory(exists){
         historyEl.appendChild(button);
     }
     
+}
+var todayEl = document.createElement("h2");
+var weatherInfo = document.createElement("ul");
+var temp = document.createElement("li");
+var wind = document.createElement("li");
+var humidity = document.createElement("li");
+function today(element){
+    var todayBox = document.getElementById("today");
+    todayDate = dayjs().format("DD/MM/YYYY");
+    todayEl.textContent = element.city.name + " " + todayDate + " ";
+    var celsius = Math.round(element.list[0].main.temp - 273.15);
+    temp.textContent = "Temp: " + celsius + " °C";
+    wind.textContent = "Wind: " + element.list[0].wind.speed + " KPH";
+    humidity.textContent = "Humidity: " + element.list[0].main.humidity + "%";
+    weatherInfo.setAttribute("class", "list-unstyled");
+    weatherInfo.appendChild(temp);
+    weatherInfo.appendChild(wind);
+    weatherInfo.appendChild(humidity);
+    todayBox.appendChild(todayEl);
+    todayBox.appendChild(weatherInfo);
+    todayBox.setAttribute("class","border border-dark mt-3")
 }
 
